b535e1f9a554ef2efd5d34a7f672c322333a3087
chivesab <chivesab@gmail.com>
2026-01-28 12:03:49 +0900

    refactor: modernize project structure and tooling

--- uploads/7af130ac75df4f91889aff9d4b29fc71.py
+++ uploads/7af130ac75df4f91889aff9d4b29fc71.py
@@ -1,14 +1,13 @@
 import time
 
+
 def main():
-  start = time.time()
-  print("Before sleep: ", time.time())
-  time.sleep(20)
-  print('Wake up after sleep: ', time.time())
-  print('Ran for: ', time.time()-start)
+    start = time.time()
+    print("Before sleep: ", time.time())
+    time.sleep(20)
+    print("Wake up after sleep: ", time.time())
+    print("Ran for: ", time.time() - start)
 
 
-
-
-if __name__ == '__main__':
-  main()
+if __name__ == "__main__":
+    main()
